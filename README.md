1744b501628873c4028eb82e50a7441d13c890da
Patrick Stillhart <patrick@stillh.art>
2024-02-19 15:35:10 +0100

    feat: :tada: init

---
+++ README.md
@@ -0,0 +1,15 @@
+# @teamnovu/eslint-config-nuxt
+This package provides a shared ESLint configuration for Nuxt.js projects.
+
+## Installation
+```bash
+pnpm i @teamnovu/eslint-config-nuxt --save-dev
+```
+
+## Usage
+```js
+// .eslintrc.js
+module.exports = {
+  extends: ['@teamnovu/nuxt'],
+}
+```
---
+++ index.js
@@ -0,0 +1,98 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:vuejs-accessibility/recommended',
+  ],
+
+  rules: {
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+    'no-console': 'off',
+    'vue/no-v-html': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      {
+        registeredComponentsOnly: false,
+        ignores: [],
+      },
+    ],
+    'vue/custom-event-name-casing': ['error', 'kebab-case'],
+    'vue/v-on-event-hyphenation': [
+      'error',
+      'always',
+      {
+        autofix: false,
+      },
+    ],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+    'vue/html-comment-content-spacing': ['error', 'always'],
+    'vue/padding-line-between-blocks': ['error', 'always'],
+    'vue/new-line-between-multi-line-property': [
+      'error',
+      {
+        minLineOfMultilineProperty: 2,
+      },
+    ],
+    'vue/no-useless-v-bind': [
+      'error',
+      {
+        ignoreIncludesComment: true,
+        ignoreStringEscape: true,
+      },
+    ],
+    'vue/v-for-delimiter-style': ['error', 'in'],
+    'vue/no-empty-component-block': ['error'],
+    'vuejs-accessibility/label-has-for': [
+      'error',
+      {
+        required: {
+          some: ['id', 'nesting'],
+        },
+      },
+    ],
+    'vue/block-lang': ['error', { script: { lang: 'ts' } }],
+    'vue/html-button-has-type': 'error',
+    'vue/block-order': ['error', { order: ['template', 'script', 'style'] }],
+    'vue/block-tag-newline': 'error',
+    'vue/max-lines-per-block': ['warn', { template: 300 }],
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'es5',
+        plugins: ['prettier-plugin-tailwindcss'],
+      },
+    ],
+  },
+
+};
