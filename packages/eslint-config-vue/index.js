dcd5fdd5309e08550ff5f7c944341ad6323219b6
Patrick Stillhart <patrick@stillh.art>
2024-09-18 14:34:37 +0200

    feat: upgrade to eslint flat config

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -1,80 +1,113 @@
-require('@rushstack/eslint-patch/modern-module-resolution')
+import pluginVueA11y from 'eslint-plugin-vuejs-accessibility'
+import tailwind from 'eslint-plugin-tailwindcss'
+import prettier from 'eslint-plugin-prettier/recommended'
+import js from '@eslint/js'
 
-module.exports = {
-  extends: ['eslint-config-airbnb-base', 'plugin:tailwindcss/recommended'],
+export default [
+  js.configs.recommended,
+  prettier,
+  ...tailwind.configs['flat/recommended'],
+  ...pluginVueA11y.configs['flat/recommended'],
 
-  root: true,
-  ignorePatterns: ['dist/', 'node_modules/', '*.json', '.temp/'],
-
-  env: {
-    browser: true,
-  },
-
-  settings: {
-    'import/resolver': {
-      typescript: {},
-      alias: {
-        map: [
-          ['@', './src'],
-          ['@images', './src/assets/images'],
-        ],
-      },
-    },
-  },
-
-  parserOptions: {
-    ecmaVersion: 2022,
-    sourceType: 'module',
-  },
-
-  rules: {
-    'func-names': 'off',
-    'no-console': 'warn',
-    'no-plusplus': 'off',
-    'no-restricted-syntax': 'off',
-    'no-unreachable': 'warn',
-    'no-unused-vars': 'warn',
-    'import/extensions': ['warn', { ts: 'never' }],
-    'import/no-extraneous-dependencies': ['warn', { devDependencies: true }],
-
-    'no-mixed-operators': ['error', { allowSamePrecedence: true }],
-    'prefer-destructuring': ['error', { array: false, object: true }],
-
-    'tailwindcss/no-custom-classname': 'off',
-  },
-
-  overrides: [
-    // vue
-    {
-      files: ['*.vue'],
-      parser: 'vue-eslint-parser',
-      parserOptions: {
-        parser: {
-          js: 'espree',
-          ts: '@typescript-eslint/parser',
-          '<template>': 'espree',
+  /*************************
+   * novu rules
+   *************************/
+  {
+    rules: {
+      // prettier
+      // *************
+      'prettier/prettier': [
+        'error',
+        {
+          semi: false,
+          singleQuote: true,
+          trailingComma: 'es5',
+          plugins: ['prettier-plugin-tailwindcss'],
         },
-      },
+      ],
 
-      extends: ['@vue/airbnb', 'plugin:vue/vue3-recommended'],
-      rules: {
-        'vue/max-attributes-per-line': ['error', { singleline: 3 }],
-        'vue/multiline-html-element-content-newline': [
-          'error',
-          {
-            allowEmptyLines: true,
+      // common
+      // *************
+      'comma-dangle': [
+        'error',
+        {
+          arrays: 'always-multiline',
+          objects: 'always-multiline',
+          imports: 'always-multiline',
+          exports: 'always-multiline',
+          functions: 'never',
+        },
+      ],
+      'no-console': 'off',
+      // conflicts with prettier (https://github.com/prettier/prettier/issues/187)
+      //'no-mixed-operators': ['error', { allowSamePrecedence: true }],
+      'prefer-destructuring': ['error', { array: false, object: true }],
+      'no-unused-vars': ['error', { args: 'none' }],
+
+      // vue
+      // *************
+      'vue/no-v-html': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/no-multiple-template-root': 'off',
+      'vue/require-default-prop': 'off',
+      'vue/component-name-in-template-casing': [
+        'error',
+        'PascalCase',
+        {
+          registeredComponentsOnly: false,
+          ignores: [],
+        },
+      ],
+      'vue/custom-event-name-casing': ['error', 'kebab-case'],
+      'vue/v-on-event-hyphenation': [
+        'error',
+        'always',
+        {
+          autofix: false,
+        },
+      ],
+      'vue/html-self-closing': [
+        'error',
+        {
+          html: {
+            void: 'any',
+            normal: 'always',
+            component: 'always',
           },
-        ],
-        'vuejs-accessibility/label-has-for': 'off',
-        'vuejs-accessibility/click-events-have-key-events': 'off',
-        'vuejs-accessibility': 'off',
-        'vuejs-accessibility/alt-text': 'warn',
-        'vue/no-v-html': 'off',
-        'vue/require-default-prop': 'off',
-        'vue/multi-word-component-names': 'off',
-        'vue/no-unused-components': 'warn',
-        'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-      },
+          svg: 'always',
+          math: 'always',
+        },
+      ],
+      'vue/html-comment-content-spacing': ['error', 'always'],
+      'vue/padding-line-between-blocks': ['error', 'always'],
+      'vue/new-line-between-multi-line-property': [
+        'error',
+        {
+          minLineOfMultilineProperty: 2,
+        },
+      ],
+      'vue/no-useless-v-bind': [
+        'error',
+        {
+          ignoreIncludesComment: true,
+          ignoreStringEscape: true,
+        },
+      ],
+      'vue/v-for-delimiter-style': ['error', 'in'],
+      'vue/no-empty-component-block': ['error'],
+      'vuejs-accessibility/label-has-for': [
+        'error',
+        {
+          required: {
+            some: ['id', 'nesting'],
+          },
+        },
+      ],
+      'vue/block-lang': ['error', { script: { lang: 'ts' } }],
+      'vue/html-button-has-type': 'error',
+      'vue/block-order': ['error', { order: ['template', 'script', 'style'] }],
+      'vue/block-tag-newline': 'error',
+      'vue/max-lines-per-block': ['warn', { template: 300 }],
     },
-  ],
-}
+  },
+]
